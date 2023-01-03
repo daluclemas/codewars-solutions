53f9eb0904e7c60e7d89e969edf24d5185cb576e
daluclemas <59066632+daluclemas@users.noreply.github.com>
2023-01-03 15:02:23 +0000

    better solution to break a camel case

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ const arr = ["1","3"];
 
 const maparr = arr.map(Number)
 console.log(maparr)
+
+// break a camel case
+function solution(string) {
+  string = string.split('').map(function (el) {
+    if (el === el.toUpperCase()) {
+      el = ' ' + el
+    }
+    return el
+  })
+  return string.join('')
+}
